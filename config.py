bdac5e347946cb71503fbee39c27337df347bd43
CesareoAguirre <cesareoaguirre@outlook.com>
2022-02-17 01:08:38 -0600

    Completadas las Katas  y config.py

--- config.py
+++ config.py
@@ -10,5 +10,14 @@ def main():
     except (BlockingIOError, TimeoutError):
         print("Filesystem under heavy load, can't complete reading configuration file")
 
+    try:
+        configuration = open('config.txt')
+    except OSError as err:
+        if err.errno ==2:
+            print(f"Couldn't find the config.txt file! {err}")
+        elif err.errno == 13:
+            print(f"Found config.txt but couldn't read it: {err}")
+
+
 if __name__ == '__main__':
     main()
